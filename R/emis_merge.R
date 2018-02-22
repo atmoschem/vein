837dbb6702ec1af49b200ab048722448ff4eeb36
ibarraespinosa <zergioibarra@gmail.com>
2018-02-22 15:35:12 -0300

    fix #67

--- R/emis_merge.R
+++ R/emis_merge.R
@@ -7,17 +7,17 @@
 #' @param pol Character. Pollutant. It is a good practice to save the rds emissions with
 #' pollutants and underline '_'.
 #' @param what Character. Word to search the emissions names, "STREETS", "DF" or
-#' whatever name.
-#' @param netsf sf. 'Spatial feature' onject with the streets. It is expected
-#' that the number of rows is equal to the number of rows of street emissions. If
-#' not, the function will stop.
+#' whatever name. It is important to include the extension .'rds'
+#' @param net 'Spatial feature' or 'SpatialLinesDataFrame' with the streets.
+#' It is expected #' that the number of rows is equal to the number of rows of
+#' street emissions. If #' not, the function will stop.
 #' @param streets Logical. If true, \code{\link{emis_merge}} will read the street
 #' emissions created with \code{\link{emis_post}} by "streets_wide", returning an
 #' object with class 'sf'. If false, it will read the emissions data-frame and
 #' rbind them.
 #' @return 'Spatial feature' of lines or a dataframe of emissions
 #' @importFrom data.table rbindlist .SD
-#' @importFrom sf st_set_geometry st_sf st_geometry
+#' @importFrom sf st_set_geometry st_sf st_geometry st_as_sf
 #' @importFrom utils glob2rx
 #' @export
 #' @examples \dontrun{
@@ -26,8 +26,8 @@
 #' }
 emis_merge <- function (path = "emi",
                         pol = "CO_",
-                        what = "STREETS",
-                        netsf,
+                        what = "STREETS.rds",
+                        net,
                         streets = T){
   x <- list.files(path = path,
                   pattern = glob2rx(paste0(pol, "*", what)),
@@ -40,19 +40,22 @@ emis_merge <- function (path = "emi",
   cat("\nReading emissions from:\n")
   print(xx)
   x_rds <- lapply(x, readRDS)
-  if(streets){
+
+    nombres <- names(x_rds[[1]])
+
+    if(streets){
     for (i in 1:length(x_rds)){
       x_rds[[i]]$id <- 1:nrow(x_rds[[i]])
     }
-    nombres <- names(sf::st_set_geometry(netsf, NULL))
     x_st <- data.table::rbindlist(x_rds)
     x_st <- as.data.frame(x_st[, lapply(.SD, sum, na.rm=TRUE),
                                by = "id",
                                .SDcols = nombres ])
-    if(nrow(x_st) != nrow(netsf)){
-      stop("Number of rows of netsf must be equal to number of rows of estimates")
+    if(nrow(x_st) != nrow(net)){
+      stop("Number of rows of net must be equal to number of rows of estimates")
     }
-    netx <- st_sf(x_st, geometry = sf::st_geometry(netsf))
+    net <- sf::st_as_sf(net)
+    netx <- st_sf(x_st, geometry = sf::st_geometry(net))
     return(netx)
   } else{
     x_st <- as.data.frame(data.table::rbindlist(x_rds))
