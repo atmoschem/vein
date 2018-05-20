60a9ca1fd5115651c9db69a23fe57729f5f17112
ibarraespinosa <zergioibarra@gmail.com>
2018-05-20 17:03:51 -0300

    update invcon

--- R/invcop.R
+++ R/invcop.R
@@ -4,6 +4,7 @@
 #'
 #' @param in_name Character; Name of current project.
 #' @param out_name Character; Name of outtput project.
+#' @param all Logical; copy ALL (and for once) or not.
 #' @param main Logical; copy or not.
 #' @param ef Logical; copy or not.
 #' @param est Logical; copy or not.
@@ -19,6 +20,7 @@
 #' }
 invcop <- function (in_name = getwd(),
                     out_name,
+                    all = FALSE,
                     main = TRUE,
                     ef = TRUE,
                     est = TRUE,
@@ -27,77 +29,85 @@ invcop <- function (in_name = getwd(),
                     veh_csv = TRUE,
                     zip = TRUE) {
   # lista carpetas
-  dirs <- list.dirs(path = in_name, full.names = T, recursive = T)
+  dirs <- list.dirs(path = in_name, full.names = TRUE, recursive = TRUE)
 
   #reemplaza in)_name con out_name
   dirs2 <- gsub(pattern = in_name, replacement = out_name, x = dirs)
 
   # crea out_name
   for(i in 1:length(dirs2)){
-    dir.create(path = dirs2[i], recursive = T)
+    dir.create(path = dirs2[i], recursive = TRUE)
   }
 
+  # all
+  if(all){
+    min <- list.files(path = paste0(in_name),
+                      pattern = "*",
+                      full.names = TRUE)
+    mout <- paste0(out_name)
+    file.copy(from = min, to = mout, overwrite = TRUE, recursive = TRUE)
+  } else {
   # main
   if(main){
     min <- list.files(path = paste0(in_name),
                       pattern = "*",
-                      full.names = T)
+                      full.names = TRUE)
     mout <- paste0(out_name)
-    file.copy(from = min, to = mout, overwrite = T, recursive = T)
+    file.copy(from = min, to = mout, overwrite = TRUE)
   }
 
   # ef
   if(ef){
     efin <- list.files(path = paste0(in_name, "/ef"),
                        pattern = "*",
-                       full.names = T)
+                       full.names = TRUE)
     efout <- paste0(out_name, "/ef")
-    file.copy(from = efin, to = efout, overwrite = T, recursive = T)
+    file.copy(from = efin, to = efout, overwrite = TRUE, recursive = TRUE)
   }
 
   # est
   if(est){
     estin <- list.files(path = paste0(in_name, "/est"),
                         pattern = "*",
-                        full.names = T)
+                        full.names = TRUE)
     estout <- paste0(out_name, "/est")
-    file.copy(from = estin, to = estout, overwrite = T, recursive = T)
+    file.copy(from = estin, to = estout, overwrite = TRUE, recursive = TRUE)
   }
 
   # network
   if(network){
     nin <- list.files(path = paste0(in_name, "/network"),
                       pattern = "*",
-                      full.names = T)
+                      full.names = TRUE)
     nout <- paste0(out_name, "/network")
-    file.copy(from = nin, to = nout, overwrite = T, recursive = T)
+    file.copy(from = nin, to = nout, overwrite = TRUE, recursive = TRUE)
   }
 
   # veh rds
   if(veh_rds){
     vin <- list.files(path = paste0(in_name, "/veh"),
                       pattern = ".rds",
-                      full.names = T)
+                      full.names = TRUE)
     vout <- paste0(out_name, "/veh")
-    file.copy(from = vin, to = vout, overwrite = T, recursive = T)
+    file.copy(from = vin, to = vout, overwrite = TRUE, recursive = TRUE)
   }
 
   # veh csv
   if(veh_csv){
     vin <- list.files(path = paste0(in_name, "/veh"),
                       pattern = ".csv",
-                      full.names = T)
+                      full.names = TRUE)
     vout <- paste0(out_name, "/veh")
-    file.copy(from = vin, to = vout, overwrite = T, recursive = T)
+    file.copy(from = vin, to = vout, overwrite = TRUE, recursive = TRUE)
   }
-
+  }
   # # emis
   # if(emis){
   #   ein <- list.files(path = paste0(in_name, "/emis"),
   #                     pattern = "*",
-  #                     full.names = T)
+  #                     full.names = TRUE)
   #   eout <- paste0(out_name, "/veh")
-  #   file.copy(from = ein, to = eout, overwrite = T, recursive = T)
+  #   file.copy(from = ein, to = eout, overwrite = TRUE, recursive = TRUE)
   # }
   # zip
   if(zip){
