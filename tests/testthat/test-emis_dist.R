e138b583b6362756efa6ffd49b44d4581d363c8f
Sergio Ibarra Espinosa <zergioibarra@gmail.com>
2018-07-13 23:47:07 -0300

    v0.4.0 🕺 🆒 🆓 🇻.0⃣.4⃣.0⃣

---
+++ tests/testthat/test-emis_dist.R
@@ -0,0 +1,14 @@
+context("emis_dist")
+data(net)
+data(pc_profile)
+po <- 1000
+t1 <- emis_dist(gy = po, spobj = net)
+t2 <- emis_dist(gy = po, spobj = net, pro = pc_profile)
+
+test_that("emis_dist works", {
+  expect_equal(as.numeric(sum(t1$gy)), 1000)
+})
+
+test_that("emis_dist works", {
+  expect_equal(as.numeric(sum(t2$h1)), 0.02947961)
+})
