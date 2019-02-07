a7422a94bb5b4132627aa48dcdce010e0725b502
ibarraespinosa <sergio.ibarra@usp.br>
2019-02-07 18:30:18 +0800

    fix #138

--- R/emis_det.R
+++ R/emis_det.R
@@ -1,56 +1,54 @@
 #' Determine deterioration factors for urban conditions
 #'
-#' @description \code{emis_det} returns deterioration factors. The emission
+#' @description \code{\link{emis_det}} returns deterioration factors. The emission
 #' factors comes from the guidelines for developing emission factors of the
 #' EMEP/EEA air pollutant emission inventory guidebook
 #' http://www.eea.europa.eu/themes/air/emep-eea-air-pollutant-emission-inventory-guidebook
 #' This function subset an internal database of emission factors with each argument
 #'
-#' @param po Pollutant
-#' @param cc Size of engine in cc
-#' @param eu Euro standard: "PRE", "I", "II", "III", "III", "IV", "V", "VI"
-#' @param km mileage in km
-#' @return It returns a numeric vector without "units"
+#' @param po Character; Pollutant "CO", "NOx" or "HC"
+#' @param cc Character; Size of engine in cc converin "<=1400", "1400_2000" or ">2000"
+#' @param eu Character; Euro standard: "PRE", "I", "II", "III", "III", "IV", "V", "VI"
+#' @param km Numeric; accumulated mileage in km
+#' @return It returns a numeric vector representing the increase in emissions due to normal deterioring
 #' @keywords deterioration emission factors
 #' @export
-#' @examples \dontrun{
+#' @examples {
 #' data(fkm)
 #' pckm <- fkm[[1]](1:24); pckma <- cumsum(pckm)
-#' cod1 <- emis_det(po = "CO", cc = 1000, eu = "III", km = pckma[1:11])
+#' (cod1 <- emis_det(po = "CO", cc = "<=1400", eu = "III", km = pckma[1:11]))
 #' }
 emis_det <- function(po, cc, eu, km) {
-  if (po == "CO" & (eu == "I" | eu == "II") &  cc <= 1400) {
+  if (po == "CO" & eu %in% c("I", "II") &  cc == "<=1400") {
     mc <- ifelse(km<120000, 1.523e-05*km+0.557, 2.39)
-  } else if (po == "CO" & (eu == "I" | eu == "II") & cc > 1400 & cc <= 2000) {
+  } else if (po == "CO" & eu %in% c("I", "II") & cc == "1400_2000") {
     mc <- ifelse(km<120000, 1.148e-05*km+0.543, 1.92)
-  } else if (po == "CO" & (eu == "I" | eu == "II") & cc > 2000) {
+  } else if (po == "CO" & eu %in% c("I", "II") & cc == ">2000") {
     mc <- ifelse(km<120000, 9.243e-06*km+0.565, 1.67)
+  } else if (po == "CO" & eu %in% c("III", "IV") & cc == "<=1400") {
+    mc <- ifelse(km<160000, 7.129e-06*km+0.769, 1.91)
+  } else if (po == "CO" & eu %in% c("III", "IV") & cc %in% c("1400_2000", ">2000")) {
+    mc <- ifelse(km<160000, 2.670e-06*km+0.955, 1.38)
 
-  } else if (po == "NOx" & (eu == "I" | eu == "II") & cc > 0) {
+  } else if (po == "NOx" & eu %in% c("I", "II") & cc %in% c("<=1400" , "1400_2000", ">2000")) {
     mc <- ifelse(km<120000, 1.598e-05*km+0.282, 2.2)
 
-  } else if (po == "HC" & (eu == "I" | eu == "II") & cc <= 1400) {
+  } else if (po == "NOx" &  eu %in% c("III", "IV") & cc == "<=1400") {
+    mc <- ifelse(km<160000, 1, 1)
+  } else if (po == "NOx" &  eu %in% c("III", "IV") & cc %in% c("1400_2000", ">2000")) {
+    mc <- ifelse(km<160000, 3.986e-06*km+0.932, 1.57)
+
+  } else if (po == "HC" & eu %in% c("I", "II") & cc == "<=1400") {
     mc <- ifelse(km<120000, 1.215e-05*km+0.647, 2.1)
-  } else if (po == "HC" & (eu == "I" | eu == "II") & cc > 1400 & cc <= 2000) {
+  } else if (po == "HC" & eu %in% c("I", "II") & cc == "1400_2000") {
     mc <- ifelse(km<120000, 1.212e-05*km+0.509, 1.99)
-  } else if (po == "HC" & (eu == "I" | eu == "II") & cc > 2000) {
+  } else if (po == "HC" & eu %in% c("I", "II") & cc == ">2000") {
     mc <- ifelse(km<120000, 1.208e-05*km+0.432, 1.88)
-
-    } else if (po == "CO" & (eu == "III" | eu == "IV") & cc < 1400) {
-    mc <- ifelse(km<160000, 7.129e-06*km+0.769, 1.91)
-    } else if (po == "CO" & (eu == "III" | eu == "IV") & cc >= 1400) {
-      mc <- ifelse(km<160000, 2.670e-06*km+0.955, 1.38)
-
-    } else if (po == "NOx" & (eu == "III" | eu == "IV") & cc < 1400) {
-      mc <- ifelse(km<160000, 1, 1)
-    } else if (po == "NOx" & (eu == "III" | eu == "IV") & cc >= 1400) {
-      mc <- ifelse(km<160000, 3.986e-06*km+0.932, 1.57)
-
-    } else if (po == "HC" & (eu == "III" | eu == "IV") & cc < 1400) {
+    } else if (po == "HC" & eu %in% c("III", "IV") & cc ==  "<=1400") {
       mc <- ifelse(km<160000, 3.419e-06*km+0.891, 1.44)
-    } else if (po == "HC" & (eu == "III" | eu == "IV") & cc >= 1400) {
+    } else if (po == "HC" & eu %in% c("III", "IV") & cc %in% c("1400_2000", ">2000")) {
       mc <- ifelse(km<160000, 1, 1)
     }
-  mc <- ifelse(mc<1,1,mc)
+  mc <- ifelse(mc < 1, 1, mc)
   return(mc)
 }
--- tests/testthat/test-emis_det.R
+++ tests/testthat/test-emis_det.R
@@ -5,7 +5,7 @@ pckm <- fkm[[1]](1:24); pckma <- cumsum(pckm)
 
 test_that("emis_det works", {
   expect_equal(emis_det(po = "CO",
-                        cc = 1000,
+                        cc = "<=1400",
                         eu = "III",
                         km = pckma[1:11])[10],
                1.756295 + 6.74e-08)
