7437b660cfecfddb630fc7816f208b451c935ccf
ibarraespinosa <sergio.ibarra@usp.br>
2019-02-07 19:05:57 +0800

    fix #138

--- tests/testthat/test-emis_det.R
+++ tests/testthat/test-emis_det.R
@@ -2,11 +2,11 @@ context("emis_det")
 data(fkm)
 pckm <- fkm[[1]](1:24); pckma <- cumsum(pckm)
 
-
+km <- units::set_units(cumsum(pckma[1:11]), km)
 test_that("emis_det works", {
   expect_equal(emis_det(po = "CO",
                         cc = "<=1400",
                         eu = "III",
-                        km = pckma[1:11])[10],
-               1.756295 + 6.74e-08)
+                        km = km)[10],
+               1.91)
 })
