5733482eb9a65a3211f4f27565e99388a5d765cc
ibarraespinosa <sergio.ibarra@usp.br>
2019-02-11 17:44:13 +0800

    improve cold_mileage

--- R/cold_mileage.R
+++ R/cold_mileage.R
@@ -7,10 +7,18 @@
 #' @param ltrip Numeric; Length of trip.
 #' @param ta Numeric; average monthly temperature
 #' @keywords cold mileage
+#' @note This function is set so that values vaires between 0 and 1.
 #' @export
 #' @examples {
 #' cold_mileage(10, 0)
+#' a <-sapply(1:50, function(i){cold_mileage(0:40, (1:50)[i])})
+#' filled.contour(a, col = cptcity::cpt(2860))
 #' }
 cold_mileage <- function(ltrip, ta){
-  0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta
+ifelse(
+  0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta < 0, 0,
+       ifelse(
+         0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta > 1, 1,
+         0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta))
+
 }
