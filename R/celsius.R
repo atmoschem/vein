5c3c215a16e2e01eba89234edf0a3fa7b9ffc57c
ibarraespinosa <sergio.ibarra@usp.br>
2019-02-20 22:00:21 +0800

    add function celsius to add celsius units to different R objects

---
+++ R/celsius.R
@@ -0,0 +1,33 @@
+#' Construction function for class "Emissions"
+#'
+#' @description \code{celsius} jsut convert add unit celsius to different
+#' R objects
+#'
+#' @return Objects of class "data.frame" or "units"
+#'
+#' @param x Object with class "data.frame", "matrix",  "numeric" or "integer"
+#' @importFrom units as_units
+#'
+#' @examples {
+#' a <- celsius(rnorm(100)*10)
+#' plot(a)
+#' b <- celsius(matrix(rnorm(100)*10, ncol = 10))
+#' print(head(b))
+#' }
+#' @export
+celsius <- function(x) {
+  if ( is.matrix(x) ) {
+    e <- as.data.frame(x)
+    for(i in 1:ncol(e)){
+      e[,i] <- e[,i]*units::as_units("degC")
+    }
+  } else if ( is.data.frame(x) ) {
+    e <- x
+    for(i in 1:ncol(x)){
+      e[,i] <- e[,i]*units::as_units("degC")
+    }
+  } else if( class(x) == "numeric" | class(x) == "integer") {
+    e <- x*units::as_units("degC")
+  }
+  return(e)
+}
--- R/cold_mileage.R
+++ R/cold_mileage.R
@@ -4,21 +4,61 @@
 #' From the guidelines  EMEP/EEA air pollutant emission inventory guidebook
 #' http://www.eea.europa.eu/themes/air/emep-eea-air-pollutant-emission-inventory-guidebook
 #'
-#' @param ltrip Numeric; Length of trip.
-#' @param ta Numeric; average monthly temperature
+#' @param ltrip Numeric; Length of trip. It must be in 'units' km.
+#' @param ta Numeric or data.frame; average monthly temperature Celsius. It if is a
+#' data.frame, it is convenient that each column is each month.
 #' @keywords cold mileage
 #' @note This function is set so that values vaires between 0 and 1.
 #' @export
 #' @examples {
-#' cold_mileage(10, 0)
-#' a <-sapply(1:50, function(i){cold_mileage(0:40, (1:50)[i])})
-#' filled.contour(a, col = cptcity::cpt(2860))
+#' lkm <- units::set_units(1:10, km)
+#' ta <- celsius(matrix(0:9, ncol = 12, nrow = 10))
+#' a <- cold_mileage(lkm, rbind(ta, ta))
+#' (a)
+#' filled.contour(as.matrix(a), col = cptcity::lucky(n = 16))
 #' }
 cold_mileage <- function(ltrip, ta){
-ifelse(
-  0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta < 0, 0,
-       ifelse(
-         0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta > 1, 1,
-         0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta))
+  # Check units
+  if(class(ltrip) != "units"){
+    stop("ltrip neeeds to has class 'units' in 'km'. Please, check package '?units::set_units'")
+  }
+  if(units(ltrip)$numerator == "m" ){
+    stop("Units of lkm is 'm'. Please, check package '?units::set_units'")
+  }
+  if(units(ltrip)$numerator == "km" ){
+    ltrip <- as.numeric(ltrip)
+  }
 
+  if(is.data.frame(ta) | is.matrix(ta)){
+    if(class(ta[, 1]) != "units") stop("ta must be untis in celsius, use units::set_units(ta, degC)")
+    ta <- as.data.frame(ta)
+    for(i in 1:ncol(ta)) {
+      ta[, i] <- as.numeric(ta[, i])
+    }
+    clkm <- as.data.frame(
+      sapply(
+        1:ncol(ta),
+        function(i){
+          ifelse(
+            0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta[, i] < 0, 0,
+            ifelse(
+              0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta[, i] > 1, 1,
+              0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta[, i]))
+        }
+      )
+    )
+  } else {
+    if(class(ta) != "units") stop("ta must be untis in celsius, use units::set_units(ta, degC)")
+    ta <- as.numeric(ta)
+
+    clkm <- ifelse(
+      0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta < 0, 0,
+      ifelse(
+        0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta > 1, 1,
+        0.6474 - 0.02545 * ltrip - (0.00974 - 0.000385 * ltrip) * ta))
+
+  }
+
+
+return(clkm)
 }
