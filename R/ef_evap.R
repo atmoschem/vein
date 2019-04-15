17a11ce781927ab0056036a92bede4f199199937
ibarraespinosa <sergio.ibarra@usp.br>
2019-04-15 21:58:59 +0800

    travis

--- R/ef_evap.R
+++ R/ef_evap.R
@@ -113,8 +113,10 @@ ef_evap <- function (ef, v, cc, dt, ca, k = 1, ltrip,  kmday, show = FALSE,
     if(units(kmday)$numerator == "km" ){
       kmday <- as.numeric(kmday)
     }
-
   }
+  # check(dt)
+    dt <- remove_units(dt)
+
 
 
   if(!is.data.frame(dt)){
@@ -192,9 +194,6 @@ ef_evap <- function (ef, v, cc, dt, ca, k = 1, ltrip,  kmday, show = FALSE,
 
   } else if (is.data.frame(dt)){
     if(is.numeric(dt[, 1])){
-      if(class(dt[, 1]) != "units"){
-        stop("if dt is not character, it must be units in celsius, use celsius(ta)")      }
-      dt <- remove_units(dt)
       for(i in 1:ncol(dt)){
         dt[, i] = ifelse(dt[, i] < a, ta,
                          ifelse(dt[, i] >=a & dt[, i] < b, tb,
--- R/remove_units.R
+++ R/remove_units.R
@@ -5,7 +5,6 @@
 #' @param x Object with class "data.frame", "matrix" or "units"
 #' @return "data.frame", "matrix" or numeric
 #' @keywords units
-#' @importFrom units drop_units
 #' @export
 #' @examples {
 #' ef1 <- ef_cetesb(p = "CO", c("PC_G", "PC_FE"))
@@ -19,18 +18,21 @@ remove_units <- function(x){
   if ( is.matrix(x) ) {
     ef <- as.data.frame(x)
     for(i in 1:ncol(ef)){
-      ef[,i] <- units::drop_units(ef[,i])
+      ef[,i] <- as.numeric(ef[,i])
     }
     class(ef) <- "matrix"
-    efx <- ef
   } else if ( is.data.frame(x) ) {
     ef <- x
     for(i in 1:ncol(ef)){
-      ef[,i] <- units::drop_units(ef[,i])
+      ef[,i] <- as.numeric(ef[,i])
     }
     class(ef) <- "data.frame"
-  } else if( class(x) == "units") {
-    ef <- units::drop_units(x)
+  } else if(is.character(x) | is.list(x) | is.array(x)){
+    ef <- x
+    #nada
+  } else {
+    ef <- x
+    ef <- as.numeric(x)
   }
   return(ef)
 }
