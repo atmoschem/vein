161aff118ca2df61b1db0d57c1ae007750acb402
ibarraespinosa <sergio.ibarra@usp.br>
2019-06-21 15:29:45 +0800

    add replace of NA with zeroes in age* functions

--- R/age.R
+++ R/age.R
@@ -147,6 +147,9 @@ if(!missing(agemax)) x <- x[1:agemax]
     )
     cat("\n")
   }
+# replace NA and NaN
+df[is.na(df)] <- 0
+
   if(!missing(net)){
     netsf <- sf::st_as_sf(net)
     dfsf <- sf::st_sf(Vehicles(df), geometry = netsf$geometry)
