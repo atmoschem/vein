e74afe9928b9d140589874dd38d4a50f98590bc9
ibarraespinosa <sergio.ibarra@usp.br>
2019-10-09 20:26:53 -0300

    covr

--- tests/testthat/test-ef_hdv_speed.R
+++ tests/testthat/test-ef_hdv_speed.R
@@ -4,3 +4,15 @@ test_that("ef_hdv_speed works", {
   expect_equal(ef_hdv_speed(v = "Trucks",t = "RT", g = "<=7.5", e = "II", gr = 0,
                             l = 0.5, p = "HC", show.equation = FALSE)(30), 0.207398745)
 })
+
+test_that("ef_hdv_speed works", {
+  expect_equal(round(ef_hdv_speed(v = "Trucks",
+                            t = "RT",
+                            g = "<=7.5",
+                            e = data.frame(eu = "II"),
+                            gr = 0,
+                            l = 0.5,
+                            p = "HC",
+                            speed = Speed(0))[1,1]),
+               EmissionFactors(1))
+})
