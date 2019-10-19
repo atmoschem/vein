f60596643dd0713abd9373822298cbf323142a1c
ibarraespinosa <sergio.ibarra@usp.br>
2019-10-18 22:10:01 -0300

    covr

--- tests/testthat/test-emis_to_streets.R
+++ tests/testthat/test-emis_to_streets.R
@@ -1,9 +1,27 @@
 context("emis_to_streets")
 
-test_that("emis_merge works", {
-  expect_error(emis_merge(),
-                ".?")
+data(net)
+stpro = data.frame(stpro = as.character(unique(net$tstreet)),
+                   VAL = 1:9)
+dnet <- net["ldv"]
+dnet$stpro <- as.character(net$tstreet)
+dnet$ID <- "A"
+df2 <- data.frame(BC = 10, CO = 20, ID = "A")
 
+
+test_that("emis_to_streets works", {
+  expect_equal(round(emis_to_streets(streets = dnet,
+                                     dfemis = df2)$BC[1]),
+                0)
+  expect_message(round(emis_to_streets(streets = dnet,
+                                     dfemis = df2)$BC[1]),
+               "f.?")
+
+
+    expect_equal(round(emis_to_streets(streets = dnet, dfemis = df2, stpro = stpro)$BC[1]),
+               0)
+    expect_message(round(emis_to_streets(streets = dnet, dfemis = df2, stpro = stpro)$BC[1]),
+                 "f.?")
 })
 
 
