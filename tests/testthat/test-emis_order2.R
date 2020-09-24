e3ebac89b0e5e999be7f85f1d14203631494eb49
ibarraespinosa <sergio.ibarra@usp.br>
2020-09-24 12:42:59 -0300

    travis

--- tests/testthat/test-emis_order2.R
+++
@@ -1,7 +0,0 @@
-context("emis_post")
-
-df <- as.data.frame(matrix(1:100, ncol =10, nrow = 10))
-wCO <- emis_order2(x = df,
-                    lt_emissions = "2020-02-19 00:00",
-                    start_utc_time = "2020-02-20 00:00",
-                    desired_length = 78)
