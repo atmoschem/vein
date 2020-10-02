007ab0a379ee56784ce7854d32f1c363fd40c8c7
ibarraespinosa <sergio.ibarra@usp.br>
2020-10-02 16:12:05 -0300

    fix cbp5 for voc and travis

--- projects/brazil_bu_cb05/scripts/cb05.R
+++ projects/brazil_bu_cb05/scripts/cb05.R
@@ -71,95 +71,95 @@ voc[is.na(voc)] <- 0
 names(voc)
 
 # eth ####
-g_eth <- g
-g_eth <- merge(g, voc[["e_eth"]], by="id", all=T)
-g_eth <- st_sf(g_eth, geometry = g$geometry)
-saveRDS(g_eth, file="post/spec_grid/E_ETH.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_eth"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_ETH.rds")
 
 # hc3 ####
-g_hc3 <- g
-g_hc3 <- merge(g, voc[["e_hc3"]], by="id", all=T)
-g_hc3 <- st_sf(g_hc3, geometry = g$geometry)
-saveRDS(g_hc3, file="post/spec_grid/E_HC3.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_hc3"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_HC3.rds")
 
 # hc5 ####
-g_hc5 <- g
-g_hc5 <- merge(g, voc[["e_hc5"]], by="id", all=T)
-g_hc5 <- st_sf(g_hc5, geometry = g$geometry)
-saveRDS(g_hc5, file="post/spec_grid/E_HC5.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_hc5"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_HC5.rds")
 
 # hc8 ####
-g_hc8 <- g
-g_hc8 <- merge(g, voc[["e_hc8"]], by="id", all=T)
-g_hc8 <- st_sf(g_hc8, geometry = g$geometry)
-saveRDS(g_hc8, file="post/spec_grid/E_HC8.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_hc8"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_HC8.rds")
 
 # ol2 ####
-g_ol2 <- g
-g_ol2 <- merge(g, voc[["e_ol2"]], by="id", all=T)
-g_ol2 <- st_sf(g_ol2, geometry = g$geometry)
-saveRDS(g_ol2, file="post/spec_grid/E_OL2.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_ol2"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_OL2.rds")
 
 # olt ####
-g_olt <- g
-g_olt <- merge(g, voc[["e_olt"]], by="id", all=T)
-g_olt <- st_sf(g_olt, geometry = g$geometry)
-saveRDS(g_olt, file="post/spec_grid/E_OLT.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_olt"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_OLT.rds")
 
 # oli ####
-g_oli <- g
-g_oli <- merge(g, voc[["e_oli"]], by="id", all=T)
-g_oli <- st_sf(g_oli, geometry = g$geometry)
-saveRDS(g_oli, file="post/spec_grid/E_OLI.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_oli"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_OLI.rds")
 
 # iso ####
-g_iso <- g
-g_iso <- merge(g, voc[["e_iso"]], by="id", all=T)
-g_iso <- st_sf(g_iso, geometry = g$geometry)
-saveRDS(g_iso, file="post/spec_grid/E_ISO.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_iso"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_ISO.rds")
 
 # tol ####
-g_tol <- g
-g_tol <- merge(g, voc[["e_tol"]], by="id", all=T)
-g_tol <- st_sf(g_tol, geometry = g$geometry)
-saveRDS(g_tol, file="post/spec_grid/E_TOL.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_tol"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_TOL.rds")
 
 # xyl ####
-g_xyl <- g
-g_xyl <- merge(g, voc[["e_xyl"]], by="id", all=T)
-g_xyl <- st_sf(g_xyl, geometry = g$geometry)
-saveRDS(g_xyl, file="post/spec_grid/E_XYL.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_xyl"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_XYL.rds")
 
 # ket ####
-g_ket <- g
-g_ket <- merge(g, voc[["e_ket"]], by="id", all=T)
-g_ket <- st_sf(g_ket, geometry = g$geometry)
-saveRDS(g_ket, file="post/spec_grid/E_KET.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_ket"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_KET.rds")
 
 # ch3oh ####
-g_ch3oh <- g
-g_ch3oh <- merge(g, voc[["e_ch3oh"]], by="id", all=T)
-g_ch3oh <- st_sf(g_ch3oh, geometry = g$geometry)
-saveRDS(g_ch3oh, file="post/spec_grid/E_CH3OH.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_ch3oh"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_CH3OH.rds")
 
 # c2h5oh ####
-g_c2h5oh <- g
-g_c2h5oh <- merge(g, voc[["e_c2h5oh"]], by="id", all=T)
-g_c2h5oh <- st_sf(g_ch3oh, geometry = g$geometry)
-saveRDS(g_c2h5oh, file="post/spec_grid/E_C2H5OH.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_c2h5oh"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_C2H5OH.rds")
 
 
 # hcho ####
-g_hcho <- g
-g_hcho <- merge(g, voc[["e_hcho"]], by="id", all=T)
-g_hcho <- st_sf(g_ch3oh, geometry = g$geometry)
-saveRDS(g_hcho, file="post/spec_grid/E_HCHO.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_hcho"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_HCHO.rds")
 
 # ald ####
-g_ald <- g
-g_ald <- merge(g, voc[["e_ald"]], by="id", all=T)
-g_ald <- st_sf(g_ch3oh, geometry = g$geometry)
-saveRDS(g_ald, file="post/spec_grid/E_ALD.rds")
+g_x <- g
+g_x <- merge(g, voc[["e_ald"]], by="id", all=T)
+g_x <- st_sf(g_x, geometry = g$geometry)
+saveRDS(g_x, file="post/spec_grid/E_ALD.rds")
 
 # CO  ####
 x <- st_set_geometry(readRDS("post/grids/CO.rds"), NULL)
