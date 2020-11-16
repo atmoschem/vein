a3593adefbff6aaff1d916dcc4d8b56f06c763a0
ibarraespinosa <sergio.ibarra@usp.br>
2020-11-16 09:34:32 -0300

    ef_cetesb

--- R/ef_cetesb.R
+++ R/ef_cetesb.R
@@ -184,29 +184,30 @@ ef_cetesb <- function(p,
              "LCV_D")
     # CO HC NMHC
     COHC <- c("CO", "CO_0km","NMHC", "NMHC_0km","HC", "HC_0km")
-    ef[ef$Pollutant %in% COHC, LDV] <- ef[ef$Pollutant %in% COHC, LDV]*1.842674
+    ef[ef$Pollutant %in% COHC, LDV] <- ef[ef$Pollutant %in% COHC, LDV]*5.823289
 
-    ef[ef$Pollutant %in% COHC, HDV] <- ef[ef$Pollutant %in% COHC, HDV]*2.905313
+    ef[ef$Pollutant %in% COHC, HDV] <- ef[ef$Pollutant %in% COHC, HDV]*2.359374
 
     # NONO2 NOx
     NONO<- c("NOx", "NO2", "NO", "NOx_0km", "NO2_0km", "NO_0km")
-    ef[ef$Pollutant %in% NONO, HDV] <- ef[ef$Pollutant %in% NONO, HDV]*1.459931
+    ef[ef$Pollutant %in% NONO, LDV] <- ef[ef$Pollutant %in% NONO, LDV]*1.405462
+    ef[ef$Pollutant %in% NONO, HDV] <- ef[ef$Pollutant %in% NONO, HDV]*1.188174
 
     # PM
-    ef[ef$Pollutant %in% "PM", HDV] <- ef[ef$Pollutant %in% "PM", HDV]*1.487109
+    ef[ef$Pollutant %in% "PM", LDV] <- ef[ef$Pollutant %in% "PM", LDV]*1.38637
 
     # ALD # ja tem NMHC incrementado
     efALD <- ef[ef$Pollutant %in% "NMHC", ]
     efALD$Pollutant <- "ALD"
-    efALD[, LDV] <- efALD[, LDV]*0.02420901
-    efALD[, HDV] <- efALD[, HDV]*0.08494866
+    efALD[, LDV] <- efALD[, LDV]*0.06693206
+    efALD[, HDV] <- efALD[, HDV]*0.05016216
     ef <- rbind(ef, efALD)
 
     # HCHO # ja tem NMHC incrementado
     efHCHO <- ef[ef$Pollutant %in% "NMHC", ]
     efHCHO$Pollutant <- "HCHO"
-    efHCHO[, LDV] <- efHCHO[, LDV]*0.01864748
-    efHCHO[, HDV] <- efHCHO[, HDV]*0.08494866
+    efHCHO[, LDV] <- efHCHO[, LDV]*0.05155577
+    efHCHO[, HDV] <- efHCHO[, HDV]*0.05016216
     ef <- rbind(ef, efHCHO)
 
   }
