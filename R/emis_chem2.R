ebc0ca88dacc49b9cef50befcbfedbc13330a616
ibarraespinosa <sergio.ibarra@usp.br>
2020-12-10 11:33:01 -0300

    update emis_chem2

--- R/emis_chem2.R
+++ R/emis_chem2.R
@@ -6,6 +6,7 @@
 #' @param df data.frame with emissions including columns "id" and "pol".
 #' @param mech Character, "CB4", "CB05", soon: "S99", "S7", "S7T", "S11", "MOZT1"
 #' @param nx Character, colnames for emissions data, for instance "V1", "V2"...
+#' @param na.rm Logical, to remove lines with NA from group
 #' @return data.frame with lumped groups by chemical mechanism.
 #' @importFrom data.table setDF as.data.table setDT setorderv melt
 #' @seealso \code{\link{speciate}}
@@ -16,39 +17,56 @@
 #' @examples \dontrun{
 #' # experimental
 #' }
-emis_chem2 <- function(df, mech, nx) {
+emis_chem2 <- function(df, mech, nx, na.rm = FALSE) {
   chem <- sysdata$chem
-  df$pol <- ifelse(df$pol == "isopentane", "2-methyl-butane",
-                   ifelse(df$pol == "ethanol", "ethyl alcohol",
-                          ifelse(
-                            df$pol == "propene", "propylene",
-                            df$pol)))
+  if(!any(grepl("id", names(df))))stop("Add 'id' column")
 
-  names(chem)[2] <- "pol"
+  id <- df$id
+
   if(missing(nx)) stop("Add colnames of emissions data")
   data.table::setDT(chem)
-  pol <- NULL
+  pol <- mol<- NULL
   if(mech == "CB05") {
-    cheml <- data.table::melt(data = chem[pol %in% unique(df[["pol"]])],
-                              id.vars = c("ID", "pol", "Mwt"),
-                              measure.vars = grep(pattern = "CB05", x = names(chem), value = TRUE),
-                              variable.name = "CB05",
-                              value.name = "mol",
-                              na.rm = TRUE)
+    cheml <- suppressWarnings(
+      data.table::melt(
+        data = chem[pol %in% unique(df[["pol"]])],
+        id.vars = c("ID", "pol", "Mwt"),
+        measure.vars = grep(pattern = "CB05",
+                            x = names(chem),
+                            value = TRUE),
+        variable.name = "CB05",
+        value.name = "mol",
+        na.rm = TRUE,
+        verbose = FALSE
+      ))
+    cheml <- cheml[mol > 0]
+
   } else if(mech == "CB4"){
-    cheml <- data.table::melt(data = chem[pol %in% unique(df[[pol]])],
-                              id.vars = c("ID", "pol", "Mwt"),
-                              measure.vars = grep(pattern = "CB4", x = names(chem), value = TRUE),
-                              variable.name = "CB4",
-                              value.name = "mol",
-                              na.rm = TRUE)
+    cheml <- suppressWarnings(
+      data.table::melt(
+        data = chem[pol %in% unique(df[["pol"]])],
+        id.vars = c("ID", "pol", "Mwt"),
+        measure.vars = grep(pattern = "CB4",
+                            x = names(chem),
+                            value = TRUE),
+        variable.name = "CB4",
+        value.name = "mol",
+        na.rm = TRUE,
+        verbose = FALSE
+      ))
+    cheml <- cheml[mol > 0]
+
   } else {
-    nx <- c("ID", "pol", "Mwt", mech)
-    cheml <- cheml[,mech]
-    cheml <- cheml[!is.na(cheml[[3]])] #TODO Check
+    ..nd <- NULL
+    nd <- c("ID", "pol", "Mwt", mech, paste0("F", mech))
+    cheml <- chem[pol %in% unique(df[["pol"]]), ..nd]
+    names(cheml)[length(cheml)] <- "mol"
+    cheml <- cheml[!is.na(cheml[[mech]])] #TODO Check
+
   }
 
-
+  # important
+  # df$id <- rep(id, length(unique(df$pol)))
 
   data.table::setDF(df)
   data.table::setDF(cheml)
@@ -57,17 +75,24 @@ emis_chem2 <- function(df, mech, nx) {
              y = cheml,
              by = "pol",
              all.x = T)
-
-  y[, nx] <- y[, nx]/y$Mwt*y$mol
+  # key!
+  for(i in seq_along(nx)) {
+    y[[nx[i]]] <- y[[nx[i]]]/y$Mwt*y$mol
+  }
 
   data.table::setDT(y)
 
   y[[mech]] <- gsub(pattern = mech, replacement = "", x = y[[mech]])
-  id <- CB05 <- NULL
+  y[[mech]] <- gsub(pattern = "_", replacement = "", x = y[[mech]])
+
+  id <-  NULL
   dy <- y[,
           lapply(.SD, sum, na.rm = T),
           .SDcols = nx,
-          by = list(id, CB05)]
-  data.table::setorderv(dy, cols = "id")
+          by = list(id, group = get(mech))]
+  data.table::setorderv(dy, c("group", "id"))
+
+  group <- NULL
+  if(na.rm) dy <- dy[!is.na(group)]
   return(dy)
 }
