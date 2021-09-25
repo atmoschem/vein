738f33c655bffb9af22ab129aee76153d41998be
Sergio Ibarra Espinosa <zergioibarra@gmail.com>
2021-09-25 08:31:42 -0300

    add Ecuador project
    only for so far

---
+++ projects/ecuador_td_hot/scripts/exhaust_eea.R
@@ -0,0 +1,175 @@
+file.remove("emi/exhaust.csv")
+
+# switch(language,
+#         "portuguese" = cat("Plotando EF\n"),
+#         "english" = cat("Plotting EF\n"),
+#         "spanish" = cat("Plotando EF\n")
+# )
+# 
+# # emission factors
+# n_PC <- metadata[metadata$family == "PC", ]$vehicles
+# n_LCV <- metadata[metadata$family == "LCV", ]$vehicles
+# n_TRUCKS <- metadata[metadata$family == "TRUCKS", ]$vehicles
+# n_BUS <- metadata[metadata$family == "BUS", ]$vehicles
+# n_MC <- metadata[metadata$family == "MC", ]$vehicles
+# 
+# ns <- c("PC", "LCV", "TRUCKS", "BUS", "MC")
+# ln <- list(n_PC, n_LCV, n_TRUCKS, n_BUS, n_MC)
+# 
+# for (i in seq_along(ns)) {
+#         dl <- lapply(seq_along(pol), function(j) {
+#           
+#           if(metadata$v_eea_old[i] %in% c("PC", "LCV", "Motorcycle")) {
+#             
+#             ef <- ef_ldv_speed(v = metadata$v_eea_old[i],
+#                                t = metadata$t_eea_old[i],
+#                                cc = metadata$cc_eea_old[i],
+#                                f = metadata$fuel_eea_old[i],   
+#                                p = "FC",
+#                                eu = cate,
+#                                speed = Speed(metadata$speed[i]))
+#             
+#           } else {
+#             ef <- ef_hdv_speed(v = metadata$v_eea_old[i],
+#                                t = metadata$t_eea_old[i],
+#                                g = metadata$cc_eea_old[i],
+#                                eu = cate,
+#                                gr = 0,
+#                                l = 0.5,
+#                                p = "FC",
+#                                speed = Speed(metadata$speed[i]))
+#           }
+#           
+#                 data.frame(as.vector(ef)[1:40],
+#                 pol = pol[j]
+#                 )
+#         })
+# 
+#         dl <- do.call("rbind", dl)
+#         df <- wide_to_long(
+#                 df = dl,
+#                 column_with_data = ln[[i]],
+#                 column_fixed = "pol"
+#         )
+#         names(df) <- c("ef", "pol", "veh")
+#         df$age <- 1:40
+#         p <- ggplot(
+#                 df[df$ef > 0, ],
+#                 aes(x = age, y = ef, colour = veh)
+#         ) +
+#                 geom_line() +
+#                 facet_wrap(~pol, scales = "free_y") +
+#                 ylim(0, NA) +
+#                 labs(y = "g/km") +
+#                 # scale_y_log10() +
+#                 theme_bw()
+# 
+#         png(
+#                 filename = paste0("images/EF_", ns[i], ".png"),
+#                 width = 2100, height = 1500, units = "px", pointsize = 12,
+#                 bg = "white", res = 300
+#         )
+#         print(p)
+#         dev.off()
+# }
+# switch(language,
+#         "portuguese" = message("\nFiguras em /images\n"),
+#         "english" = message("\nFigures in /image\n"),
+#         "spanish" = message("\nFiguras en /images\n")
+# )
+
+# Escapamento ####
+switch(language,
+        "portuguese" = cat("Estimando emissões\n"),
+        "english" = cat("Estimating emissions\n"),
+        "spanish" = cat("Estimando emisiones\n")
+)
+metadata_original <- metadata
+metadata <- metadata[metadata$fuel != "ELEC", ]
+
+# Exhaust ####
+for (i in seq_along(metadata$vehicles)) {
+        cat(
+                "\n", metadata$vehicles[i],
+                rep("", max(nchar(metadata$vehicles) + 1) - nchar(metadata$vehicles[i]))
+        )
+
+        x <- readRDS(paste0("veh/", metadata$vehicles[i], ".rds"))
+
+        # euro
+        cate <- as.character(as.roman(gsub("Euro ", "", euro[[metadata$vehicles[i]]])))
+        cate[is.na(cate)] <- "PRE"
+        
+        for (j in seq_along(pol)) {
+                cat(pol[j], " ")
+          if(metadata$v_eea_old[i] %in% c("PC", "LCV", "Motorcycle")) {
+            
+            ef <- ef_ldv_speed(v = metadata$v_eea_old[i],
+                               t = metadata$t_eea_old[i],
+                               cc = metadata$cc_eea_old[i],
+                               f = if(metadata$fuel_eea_old[i] == "LPG" & pol[j] == "PM") "G" else  metadata$fuel_eea_old[i],  
+                               p = pol[j],
+                               eu = cate,
+                               speed = Speed(metadata$speed[i]))
+            
+            if(metadata$fuel_eea_old[i] == "LPG" & pol[j] == "PM") ef <- ef*0
+              
+          } else {
+            ef <- ef_hdv_speed(v = metadata$v_eea_old[i],
+                               t = metadata$t_eea_old[i],
+                               g = metadata$cc_eea_old[i],
+                               eu = cate,
+                               gr = 0,
+                               l = 0.5,
+                               p = pol[j],
+                               speed = Speed(metadata$speed[i]))
+          }
+          
+                array_x <- emis_hot_td(
+                        veh = x,
+                        lkm = mileage[[metadata$vehicles[i]]],
+                        ef = as.vector(ef)[1:ncol(x)],
+                        # pro_month = pmonth[[metadata$vehicles[i]]],
+                        fortran = TRUE,
+                        nt = check_nt() / 2,
+                        verbose = verbose,
+                        params = list(
+                                veh = metadata$vehicles[i],
+                                size = metadata$size[i],
+                                fuel = metadata$fuel[i],
+                                pollutant = pol[j],
+                                type_emi = "Exhaust",
+                                subtype_emi = "Exhaust",
+                                baseyear = year
+                        )
+                )
+                fwrite(array_x, "emi/exhaust.csv", append = TRUE)
+        }
+}
+
+
+switch(language,
+        "portuguese" = message("\n\nArquivos em: /emi/exhaust.csv:"),
+        "english" = message("\n\nFiles in: /emi/exhaust.csv"),
+        "spanish" = message("\n\nArchivos en: /emi/exhaust.csv")
+)
+
+
+switch(language,
+        "portuguese" = message("Limpando..."),
+        "english" = message("Cleaning..."),
+        "spanish" = message("Limpiando...")
+)
+
+suppressWarnings(
+        rm(
+                i, j, pol,
+                n_PC, n_LCV, n_TRUCKS, n_BUS, n_MC,
+                ns, ln, p, df, dl, cores
+        )
+)
+
+ls()
+
+
+invisible(gc())
---
+++ projects/ecuador_td_hot/scripts/fuel_eval.R
@@ -0,0 +1,84 @@
+file.remove("emi/FC.csv")
+# Exhaust ####
+for (i in seq_along(metadata$vehicles)) {
+  cat(
+    "\n", metadata$vehicles[i],
+    rep("", max(nchar(metadata$vehicles) + 1) - nchar(metadata$vehicles[i]))
+  )
+
+  x <- readRDS(paste0("veh/", metadata$vehicles[i], ".rds"))
+  meta <- metadata[i, ]
+  
+  for (j in seq_along(pol)) {
+    cat(pol[j], " ")
+
+    ef <- ef_cetesb(
+      p = pol[j],
+      veh = metadata$vehicles[i],
+      year = year,
+      agemax = ncol(x),
+      verbose = verbose
+    )
+
+    array_x <- emis_hot_td(
+      veh = x,
+      lkm = mileage[[metadata$vehicles[i]]],
+      ef = ef,
+      fortran = TRUE,
+      nt = check_nt() / 2,
+      pro_month = pmonth[[metadata$vehicles[i]]],
+      verbose = verbose,
+      params = list(
+        veh = metadata$vehicles[i],
+        size = metadata$size[i],
+        fuel = metadata$fuel[i],
+        pollutant = pol[j],
+        type_emi = "Exhaust",
+        subtype_emi = "Exhaust",
+        baseyear = year
+      )
+    )
+
+    fwrite(array_x, "emi/FC.csv", append = TRUE)
+  }
+}
+
+switch(language,
+  "portuguese" = message("\nArquivos em:"),
+  "english" = message("\nFiles in:"),
+  "spanish" = message("\nArchivos en:")
+)
+
+cat(paste0(getwd(), "/emi/*\n"))
+
+# data.table ####
+dt <- fread("emi/FC.csv")
+
+dt$pollutant <- as.character(dt$pollutant)
+dt$g <- units::set_units(dt$emissions, "g")
+dt$t <- units::set_units(dt$g, t)
+
+dt0 <- dt[pollutant == "FC",
+  round(sum(t), 2),
+  by = .(fuel)
+]
+data.table::setkey(dt0, "fuel")
+
+names(dt0)[2] <- "estimation_t"
+dtf <- merge(dt0, fuel, by = "fuel")
+dtf$density_tm3 <- units::set_units(dtf$density_tm3, "t/m^3")
+dtf$consumption_lt <- units::set_units(dtf$consumption_lt, "l")
+dtf$consumption_m3 <- units::set_units(dtf$consumption_lt, "m^3")
+dtf$consumption_t <- dtf$consumption_m3 * dtf$density_tm3
+dtf$estimation_consumption <- dtf$estimation_t / dtf$consumption_t
+print(dtf[, c("fuel", "estimation_t", "consumption_t", "estimation_consumption")])
+
+switch(language,
+  "portuguese" = message("Limpando..."),
+  "english" = message("Cleaning..."),
+  "spanish" = message("Limpiando...")
+)
+suppressWarnings(rm(i, j, pol, dt, dt0, dtf, factor_emi, fuel))
+
+ls()
+invisible(gc())
