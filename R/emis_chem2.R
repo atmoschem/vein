a5ce67d0bda4895a8f1e3902ad2b597155db5a34
Sergio Ibarra Espinosa <zergioibarra@gmail.com>
2022-11-02 01:04:33 -0600

    still need to fix

--- R/emis_chem2.R
+++ R/emis_chem2.R
@@ -111,12 +111,11 @@ emis_chem2 <- function(df, mech, nx, na.rm = FALSE) {
       # if(verbose) print(head(cheml))
     } else {
       # ..nd <- NULL
-      # nd <- c("ID", "pol", "Mwt", mech, paste0("F", mech))
       # cheml <- suppressWarnings(chem[pol %in% unique(df[["pol"]]), ..nd])
       # names(cheml)[length(cheml)] <- "mol"
       # cheml <- cheml[!is.na(cheml[[mech]])] #TODO Check
 
-      nd <- NULL
+      nd <- c("ID", "pol", "Mwt", mech, paste0("F", mech))
       cheml <- chem[,
                     nd,
                     with = FALSE]
