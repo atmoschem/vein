84fdd7c53d9624989717218fa26d38f3d7a7887d
sergio <zergioibarra@gmail.com>
2023-06-11 23:35:49 -0600

    add proj br

--- projects/brazil/config/05_pre_main.R
+++ projects/brazil/config/05_pre_main.R
@@ -4,20 +4,22 @@
 #saveRDS(a, "rds/ufs.rds")
 a  <- readRDS("rds/ufs.rds")
 
-ufs <- a$abbrev_state
+ufs <- a$abbrev_state |> as.character()
 uf <- "config/UF.tar.gz"
 #dir.create("estimation")
 unlink("estimation", recursive = T)
 dir.create("estimation")
 
 
-lapply(1:2, function(i) {
-  lapply(seq_along(1), function(j) {
-    print(paste0("estimation/", 1970:2100, "/", ufs[j])[i])
+years <- c(2000, 2022)
+
+lapply(seq_along(years), function(i) {
+  lapply(seq_along(ufs), function(j) {
+    print(paste0("estimation/", years, "/", ufs[j])[i])
 
     untar(tarfile = uf, 
-          exdir = paste0("estimation/", 1970:2100, "/", 
+          exdir = paste0("estimation/", years, "/", 
                          ufs[j])[i])
-    file.remove(paste0("estimation/", 1970:2100, "/", ufs[j], "/UF.tar.gz")[i])
+    file.remove(paste0("estimation/", years, "/", ufs[j], "/UF.tar.gz")[i])
   })
 })
--- projects/brazil/config/clean_all.R
+++ projects/brazil/config/clean_all.R
@@ -1,2 +1,9 @@
 a <- list.files(path = "config", pattern = ".rds", full.names = T)
 file.remove(a)
+
+unlink("estimation", recursive = T)
+
+
+system(paste0("tar -caf ", basename(getwd()), ".tar.gz ."))
+system(paste0("mv ", basename(getwd()), ".tar.gz ../"))
+# file.remove(".Rhistory")
